42b173910361b03c276a15f50e2846de89f62c84
Silas Vasconcelos <75582929+s-v7@users.noreply.github.com>
2023-03-07 09:42:10 -0300

    UpdateComments
    updatdeComments - ElectricalMachine

--- src/models/ElectricalMachine.ts
+++ src/models/ElectricalMachine.ts
@@ -1,16 +1,17 @@
 import { ObjectId } from "mongodb";
 
 export default class ElectricalMachine {
+    //Defining Object Attributes/Properties
     public _id: ObjectId;
     public _typeMachine: string;
     public _stateMachine: boolean;
-    public _temperatura: number
+    public _temperature: number
 
-    //constructor() { }
-    constructor(id: ObjectId, typeMachine: string, stateMachine: boolean, temperatura: number) {
+    //constructor() { }  Optional 
+    constructor(id: ObjectId, typeMachine: string, stateMachine: boolean, temperature: number) {
         this._id = id;
         this._typeMachine = typeMachine;
         this._stateMachine = stateMachine;
-        this._temperatura = temperatura;
+        this._temperature = temperature;
     }
 }
